8bbffee0d0ac1ba519ec1f8e44de1b7be1199619
redflameken2 <202301178@iacademy.edu.ph>
2024-08-06 21:08:17 +0800

    fix installScript

--- .scripts/installScript.sh
+++ .scripts/installScript.sh
@@ -9,7 +9,7 @@ makepkg -si
 cd $oldDir
 
 # pacman installs
-sudo pacman -S --noconfirm sway i3 rofi tmux neovim kitty picom keyd fastfetch dunst github-cli ttf-firacode-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common stow discord qt6ct firefox kritta kdenlive obs-studio steam lxappearacne thunar thunar-volman imagemagick gthumb wine winetricks g++ jdk8-openjdk jdk17-openjdk jdk21-openjdk android-tools scrcpy reflector vlc mpv zsh zsh-autosuggestions zsh-syntax-highlighting slurp grim brightnessctl swww calc xwayland
+sudo pacman -S --noconfirm sway i3 rofi tmux neovim kitty picom keyd fastfetch dunst github-cli ttf-firacode-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common stow discord qt6ct firefox krita kdenlive obs-studio steam lxappearance thunar thunar-volman imagemagick gthumb wine winetricks g++ jdk8-openjdk jdk17-openjdk jdk21-openjdk android-tools scrcpy reflector vlc mpv zsh zsh-autosuggestions zsh-syntax-highlighting slurp grim brightnessctl swww calc
 yay -Sy --noconfirm bumblebee-status cmatrix-git webcord auto-cpufreq obs-websocket-compat obs-cli ani-cli clementine
 
 # file setup 
