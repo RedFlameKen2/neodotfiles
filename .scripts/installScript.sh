6f412821df8728c88beb373d6f2213e7c107efda
redflameken2 <202301178@iacademy.edu.ph>
2024-08-07 09:14:01 +0800

    pacman uses Sy after enable multilib

--- .scripts/installScript.sh
+++ .scripts/installScript.sh
@@ -10,7 +10,7 @@ cd $oldDir
 
 sudo sed -i 's/#\(\[multilib\]\)/\1\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc//pacman.conf
 # pacman installs
-sudo pacman -S --noconfirm sway i3 rofi tmux neovim zsh zsh-autosuggestions zsh-syntax-highlighting fzf kitty dunst picom vlc mpv slurp grim brightnessctl keyd fastfetch github-cli stow discord qt6ct firefox krita kdenlive obs-studio lxappearance thunar thunar-archive-plugin thunar-volman imagemagick gthumb jdk8-openjdk jdk17-openjdk jdk21-openjdk android-tools scrcpy reflector swww calc ttf-firacode-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common wine winetricks steam
+sudo pacman -Sy --noconfirm sway i3 rofi tmux neovim zsh zsh-autosuggestions zsh-syntax-highlighting fzf kitty dunst picom vlc mpv slurp grim brightnessctl keyd fastfetch github-cli stow discord qt6ct firefox krita kdenlive obs-studio lxappearance thunar thunar-archive-plugin thunar-volman imagemagick gthumb jdk8-openjdk jdk17-openjdk jdk21-openjdk android-tools scrcpy reflector swww calc ttf-firacode-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common wine winetricks steam
 
 yay -Sy --noconfirm bumblebee-status cmatrix-git webcord auto-cpufreq obs-websocket-compat obs-cli ani-cli clementine
 
